65209b081ed5128394ffb8e0e1ba788cb6f3209b
felix <felix.janot@edu.devinci.fr>
2021-11-23 16:06:56 +0100

    :tada: Maquette finit

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector(".navbar");
+menuIcon.addEventListener("click" , function() {
+    navbar.classList.toggle("change");
+} );
+
+console.log("je suis bien co");
